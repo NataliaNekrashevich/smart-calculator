bf209d36464b5c43ea3cc0ae568a73a94dd7a566
Nekrashevich, Natalya <n.nekrashevich@a1qa.com>
2018-09-28 13:06:34 +0300

    285 test pass

--- src/index.js
+++ src/index.js
@@ -1,26 +1,76 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.stack = [];
+    this.stack.push(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.stack.push('+');
+    this.stack.push(number);    
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.stack.push('-');
+    this.stack.push(number);    
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.stack.push('*');
+    this.stack.push(number);    
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.stack.push('/');
+    this.stack.push(number);    
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    let x = this.stack.pop();    
+    function powRealization(a, b){
+      if(b == 0) return 1;
+      let n = 1;
+      for(let i = 0; i < b; i++){
+        n *= a;
+      }
+      return n;
+    }
+    this.stack.push(powRealization(x, number));
+    
+    return this;
+  }
+
+  
+
+  valueOf(){
+    let array = this.stack;
+    function firstSimbol(a, b){
+      if(!(~a || ~b)) return -1;
+      if(~a && ~b){
+        return a < b ? a : b;
+      }
+      if(~a)  return a;
+      else return b;
+    }
+    let a = array.indexOf('*');
+    let b = array.indexOf('/');
+    let i = firstSimbol(a, b) 
+    while(~i){
+      if(array[i] === '*')  array[i - 1] *= array[i + 1];
+      else  array[i - 1] /= array[i + 1];
+      array.splice(i, 2) 
+      i = firstSimbol(array.indexOf('*'), array.indexOf('/'))     
+    }
+    i=1;
+    while(array.length!==1){
+      if(array[i] === '+')  array[i - 1] += array[i + 1];
+      else  array[i - 1] -= array[i + 1];
+      array.splice(i, 2) 
+    }
+    return array[0];
   }
 }
 
